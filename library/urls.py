db3a9b2a548c1d5f0b430013663035ec62924e41
BodyuSV <bodustanislav2@gmail.com>
2024-02-21 16:11:01 +0300

    добавить меню на главную страницу

--- library/urls.py
+++ library/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import home_page, Register_user
 
 urlpatterns = [
-    path('', home_page, name='home')
+    path('', home_page, name='home'),
+    path('register', Register_user.as_view(), name='Register'),
 ]
 
-
-
+#
+#
--- library/views.py
+++ library/views.py
@@ -1,5 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 
 def home_page(request):
     return render(request, 'home_page.html', )
+
+
+class Register_user(CreateView):
+    form_class = UserCreationForm
+    template_name = 'register.html'
+    success_url = reverse_lazy('home')
