f2db9e3b104f3affef30e58b95703bf12a83ac92
BodyuSV <bodustanislav2@gmail.com>
2024-02-21 18:42:55 +0300

    Созданы модели.

--- library/models.py
+++ library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=250)
+    country = models.CharField(max_length=200)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Book_model(models.Model):
+    book_name = models.CharField(max_length=250)
+    book_file_text = models.FileField(upload_to='book')
+    book_file_audio = models.FileField(upload_to='book_audio')
+    date_add = models.DateTimeField(auto_now_add=True)
+    is_published =models.BooleanField()
+    cover = models.FileField(upload_to='book_image')
+    genre =models.ForeignKey(Genre, on_delete=models.CASCADE)
+    Author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+    audio_version = models.BooleanField()
+    text_version = models.BooleanField()
+    date_of_publication = models.DateField()
