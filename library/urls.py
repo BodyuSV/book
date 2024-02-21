60a2f969a0c396b149e5378af8276329fe827e8e
BodyuSV <bodustanislav2@gmail.com>
2024-02-21 20:15:28 +0300

    Добавлена авторизация пользователей

--- library/urls.py
+++ library/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home_page, Register_user
+from .views import home_page, Register_user, Login
 
 urlpatterns = [
     path('', home_page, name='home'),
     path('register', Register_user.as_view(), name='Register'),
+    path('login', Login.as_view(), name='login')
+
 ]
 
 #
--- library/views.py
+++ library/views.py
@@ -1,14 +1,20 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
+
 def home_page(request):
-    return render(request, 'home_page.html', )
+    return render(request, 'home_page.html', {'title': 'Главная страница'})
 
 
 class Register_user(CreateView):
     form_class = UserCreationForm
     template_name = 'register.html'
-    success_url = reverse_lazy('home')
+
+class Login(LoginView):
+    form_class = AuthenticationForm
+    template_name = 'login.html'
+
