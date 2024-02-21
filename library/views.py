7b25e35cc2238ff52685107b06530a76a621a218
BodyuSV <bodustanislav2@gmail.com>
2024-02-21 22:36:20 +0300

    Добавлен выход из учетной записи

--- library/views.py
+++ library/views.py
@@ -1,9 +1,9 @@
+from django.contrib.auth import logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.views import LoginView
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.shortcuts import render, redirect
 
+from django.views.generic import CreateView
 
 
 def home_page(request):
@@ -14,7 +14,12 @@ class Register_user(CreateView):
     form_class = UserCreationForm
     template_name = 'register.html'
 
+
 class Login(LoginView):
     form_class = AuthenticationForm
     template_name = 'login.html'
 
+
+def logout_user(request):
+    logout(request)
+    return redirect('home')
