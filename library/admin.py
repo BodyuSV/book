d5e8c659c5e2891aaf2a8f1532c57b8184d17052
BodyuSV <bodustanislav2@gmail.com>
2024-02-22 10:34:49 +0300

    Настройка админ-панели

--- library/admin.py
+++ library/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Book_model, Author, Genre
 
-# Register your models here.
+
+class Book_modelAdmin(admin.ModelAdmin):
+    list_display = ('book_name', 'book_file_text', 'book_file_audio', 'date_add', 'is_published','cover', 'gener',
+                    'Author', 'audio_version', 'text_version', 'date_of_publication')
+    list_display_links = ('book_name', 'book_file_text', 'book_file_audio', 'date_add', 'is_published','cover', 'gener',
+                    'Author', 'audio_version', 'text_version', 'date_of_publication')
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country',)
+    list_display_links = ('name', 'country',)
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    list_display_links = ('name',)
+
+
+admin.site.register(Book_model)
+admin.site.register(Author)
+admin.site.register(Genre)
--- library/urls.py
+++ library/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import home_page, Register_user, Login, logout_user, List_book
 
 urlpatterns = [
-    path('', L, name='home'),
+    path('', List_book.as_view(), name='home'),
     path('register', Register_user.as_view(), name='Register'),
     path('login', Login.as_view(), name='login'),
     path('logout', logout_user, name='logout')
